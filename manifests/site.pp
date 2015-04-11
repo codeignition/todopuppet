dff204e1cdfc865319299915f396b98880183d14
Sinister Light <me@sinisterlight.com>
2015-04-11 16:33:43 +0530

    [Shobhit] adding event handler for serf

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,10 +2,18 @@ node basenode {
   include helloworld::motd
 
   class { 'serf':
-    bind       =>  $::ipaddress_eth1,
-    rpc_addr   =>  "${::ipaddress_eth1}:7373",
-    advertise  => $::ipaddress_eth1,
-    join => ['10.0.0.10']
+    bind          =>  $::ipaddress_eth1,
+    rpc_addr      =>  "${::ipaddress_eth1}:7373",
+    advertise     => $::ipaddress_eth1,
+    join          => ['10.0.0.10'],
+    event_handler => ['/etc/serf/puppet_agent.sh'],
+  }
+
+  file { 'puppet agent event handler':
+    path    => '/etc/serf/puppet_agent.sh',
+    ensure  => present,
+    mode    => 0755,
+    content => "#!/bin/bash \n sudo puppet agent -t > /var/log/puppet-agent.log",
   }
 }
 
