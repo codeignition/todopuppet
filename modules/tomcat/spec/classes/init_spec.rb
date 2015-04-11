3bfeaf84ff5e6e8d4cd1cb9028fda267eca3ddd2
Sinister Light <me@sinisterlight.com>
2015-04-11 10:45:37 +0530

    [Shobhit] adding facts for tests

--- modules/tomcat/spec/classes/init_spec.rb
+++ modules/tomcat/spec/classes/init_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 describe 'tomcat' do
+  let(:facts) { {'osfamily' => 'Debian', 'lsbdistid' => 'debian', 'lsbdistcodename' => 'a', 'operatingsystem' => 'Ubuntu'} }
+
   context 'with defaults for all parameters' do
     it { should contain_class('java') }
   end
